2239f432e41145ad81aa8df40faecd7cb82a3583
Erangoud <egoud83@gmail.com>
2024-12-19 16:27:45 +0530

    added the contact class and attributes

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-class AddressBookMain:
+
+class Contact: #use case 2
+    def __init__(self,name,address,city,state,zip,phoneno,email):
+        self.name=name
+        self.address=address
+        self.city=city
+        self.state=state
+        self.zip=zip
+        self.phoneno=phoneno
+        self.email=email
+
+    def __str__(self):
+        return (f'{self.name},{self.address},{self.city},{self.state},{self.zip},{self.phoneno},{self.email}')
+
+class AddressBookMain: #use case 1
+    # contacts=[]
     def __init__(self):
         print("Welcome to AddressBook program ")
-     
-addressbook1=AddressBookMain()
+
+ob1=AddressBookMain() 
